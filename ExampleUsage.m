a3c98dea00e62c5cc8b85fa602b0c8e49e782761
rharsley <66779768+rharsley@users.noreply.github.com>
2022-04-29 19:46:32 +0100

    Fix line endings

--- ExampleUsage.m
+++ ExampleUsage.m
@@ -1,1 +1,25 @@
-clear all; close all;%Add the path where functions are storedaddpath("ImpulseResponse")%Duration of impulse responseduration = 1;%Sample rateFS = 1e5;%Cylinder, 1m, 0.01 cm radius bore%Define bore profilecylinder.x = [1, 2000];cylinder.S = pi*0.005^2*[1,1];cylinder.temp = 19.85;irCyl = TubeImpulseResponse(cylinder, duration, FS);impedanceCyl = abs(fft(irCyl));n = length(irCyl);figure(1)subplot(2,1,1),plot([0:n-1]/(n*FS), irCyl,'LineWidth',2);xlabel("Time(s)","Fontsize",12)ylabel("Pressure (Pa)","Fontsize",12)title("Impulse response","Fontsize",15)subplot(2,1,2),semilogy([0:n-1]*FS/n, impedanceCyl,'LineWidth',2)xlim([0 10000])xlabel("Frequency (Hz)","Fontsize",12)title("Input impedance magnitude","Fontsize",15)
+clear all; close all;
+%Add the path where functions are stored
+addpath("ImpulseResponse")
+%Duration of impulse response
+duration = 1;
+%Sample rate
+FS = 1e5;
+%Cylinder, 1m, 0.01 cm radius bore
+%Define bore profile
+cylinder.x = [1, 2000];
+cylinder.S = pi*0.005^2*[1,1];
+cylinder.temp = 19.85;
+
+irCyl = TubeImpulseResponse(cylinder, duration, FS);
+impedanceCyl = abs(fft(irCyl));
+n = length(irCyl);
+figure(1)
+subplot(2,1,1),plot([0:n-1]/(n*FS), irCyl,'LineWidth',2);
+xlabel("Time(s)","Fontsize",12)
+ylabel("Pressure (Pa)","Fontsize",12)
+title("Impulse response","Fontsize",15)
+subplot(2,1,2),semilogy([0:n-1]*FS/n, impedanceCyl,'LineWidth',2)
+xlim([0 10000])
+xlabel("Frequency (Hz)","Fontsize",12)
+title("Input impedance magnitude","Fontsize",15)
